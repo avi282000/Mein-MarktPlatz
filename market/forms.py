a2518a1de68ed4de4efd2769e6c383492ad5e6f3
avi282000 <avi.vashisht.7@gmail.com>
2021-10-09 16:37:22 +0530

    A Proper, Functioning Registration Page
    Used a few validation techniques and made a completely working registration page

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name:')
-    email = StringField(label='Email Address:')
-    password1 = PasswordField(label='Password:')
-    password2 = PasswordField(label='Confirm Password:')
+
+    def validate_username(self, username_to_check): # It's a syntax for naming a validation function. Raw terms: def validate_FIELDNAME(self, arg)
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError("Username already exists! Please try another!")
+
+    def validate_email(self, email_to_check): # It's a syntax for naming a validation function. Raw terms: def validate_FIELDNAME(self, arg)
+        email_address = User.query.filter_by(email=email_to_check.data).first()
+        if email_address:
+            raise ValidationError("An account with the same Email Address is already registered. Forgot your password?")
+
+    username = StringField(label='User Name:', validators=[Length(min=2, max=20), DataRequired()])
+    email = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create The Account')
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for, flash
+from market.models import Item, User
 from market.forms import RegisterForm
+from market import db
 
 
 @app.route('/')
@@ -21,7 +22,19 @@ def author():
     return render_template('about_author.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user1 = User(username=form.username.data,
+                     email=form.email.data,
+                     pw_hash=form.password1.data)
+        db.session.add(user1)
+        db.session.commit()
+        return redirect(url_for('market_place'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            print(f'Error Encountered! : {err_msg}')
+            flash(f'Error Encountered! : {err_msg}', category='danger')
+
     return render_template('register.html', form=form)
